d17044c083995d5e4671996b9b48f20a299967e5
Mohammed Bermime <bermime1@gmail.com>
2023-07-14 19:41:20 +0100

    Updates

---
+++ _next/static/MaSsUP5N5__rTCXu1Tm6t/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e27f071dcd00fdfa.css","static/chunks/pages/index-a2c4594a6680fa1f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
