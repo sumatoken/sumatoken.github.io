0a489e363888cb9981aed9885548c498c4b0d551
Mohammed Bermime <bermime1@gmail.com>
2023-07-14 21:16:16 +0100

    Fix grammar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Deployed soon.
+            Deploying soon.
           </p>
         </a>
       </div>
